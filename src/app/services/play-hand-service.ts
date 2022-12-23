6ca8f318088385658ca62fe348ea5df08a389048
Lucas Mufato <lucas.mufato@sytac.com>
2022-12-23 12:23:47 +0100

    add environments and backend-api-service

--- src/app/services/play-hand-service.ts
+++ src/app/services/play-hand-service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ContestResult} from "../model/contest-result";
+import {BackendApiService} from "./backend-api.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayHandService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private backendApiService: BackendApiService) { }
 
   playHand(handType:string): Observable<ContestResult>{
-    return this.http.post<ContestResult>("http://localhost:8080/computer/play-hand", {"hand_shape": handType})
+    return this.backendApiService.post("/computer/play-hand", {"hand_shape": handType})
   }
 
 
